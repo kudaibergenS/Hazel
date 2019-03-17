061b4e4757aff51ef44875295f18e919c8d11334
Sanzhar Kydaybergen <sanzhar.k.96@gmail.com>
2019-03-17 23:57:24 +0600

    added event polling

---
+++ Hazel/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,53 @@
+#include "hzpch.h"
+#include "WindowsInput.h"
+
+#include "Hazel/Application.h"
+#include "GLFW/glfw3.h"
+
+namespace Hazel {
+
+	Input* Input::s_Instance = new WindowsInput();
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		return { (float)xpos, (float)ypos };
+	}
+
+	float WindowsInput::GetMouseXImpl()
+	{
+		auto v = GetMousePositionImpl();
+		return std::get<0>(v);
+		/* TODO: C++ 17 install vs2017
+		auto[x, y] = GetMousePositionImpl();
+		return x;
+		*/
+	}
+
+	float WindowsInput::GetMouseYImpl()
+	{
+		auto v = GetMousePositionImpl();
+		return std::get<1>(v);
+		/* TODO: C++ 17 install vs2017
+		auto[x, y] = GetMousePositionImpl();
+		return y;
+		*/
+	}
+
+}
